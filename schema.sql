cf39b9209a5a2378b5e59c8856a8ed36fefa6e5e
Mark McNamara <111820731+mmcnamarainco@users.noreply.github.com>
2022-10-11 20:09:03 -0500

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -35,4 +35,41 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL, 
+	dept_no VARCHAR(4) NOT NULL,
+  	from_date DATE NOT NULL,
+  	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+);
+
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31') 
+	AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT departments.dept_name,
+     dept_manager.emp_no,
+     dept_manager.from_date,
+     dept_manager.to_date
+FROM departments
+INNER JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no;
+
+SELECT retirement_info.emp_no,
+    retirement_info.first_name,
+	retirement_info.last_name,
+    dept_emp.to_date
+FROM retirement_info 
+LEFT JOIN dept_emp
+ON retirement_info.emp_no = dept_emp.emp_no;
+
+
 select * from departments
+
+WHERE de.to_date = ('9999-01-01');
+WHERE de.to_date = ('9999-01-01');
